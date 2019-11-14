52f771dea4f83d8bb4f6199f639856f47deeb3c8
MasCair2k16 <gotgame6@msn.com>
2019-11-13 21:22:28 -1100

    stuff

---
+++ listener.sh
@@ -0,0 +1,18 @@
+#!./bin/bash -eu
+
+DIRECTORY_TO_OBSERVE="file:///home/pi/Desktop/displayToTv"       # file directory to watch
+function block_for_change {
+
+	inotifywait --recursive \				# inotify-tools is the tool I used
+	--event modify,move,create,delete \
+	$DIRECTORY_TO_OBSERVE
+
+}
+BUILD_SCRIPT=toDisplay.sh
+function build {
+	bash $BUILD_SCRIPT
+}
+build
+while block_for_change; do
+	build
+done                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
